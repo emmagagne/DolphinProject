8be91b369bcdf5789855b7e9a3087455c57d8991
Emma Gagne <emma.gagne@uvm.edu>
2021-10-06 17:01:25 -0400

    preliminaries and getting started

---
+++ DIVE.R
@@ -0,0 +1,14 @@
+# Diversity Estimator for Dolphin Repertoire
+# 10/06/2021
+# EKG
+#--------------------------------------------------------------------------
+
+# Preliminiars
+library(ggplot2)
+library(ggthemes)
+library(DivE)
+
+# "DivE requires data where each individual has been sampled randomly, 
+# independently and with an equal probability of detection, and where the 
+# underlying distribution of individuals is spatially homogeneous."
+
